7f72ed6ae562c7172b6cb29803199178c29110ee
김범진 <bjkim9736@naver.com>
2022-12-09 01:08:36 +0900

    first commit

---
+++ hex.js
@@ -0,0 +1,19 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// #f1505 >> #F1505
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  //  문자열의 조합으로 숫자를 hexcolor로 불러오는게 효율적이다.
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
